8955262cd31251de6c612e955c418df8aac3202b
Joy <soniqjjj@gmail.com>
2026-01-28 18:07:13 +0100

    Add kangaroo function to check for meeting point
    Implement the kangaroo function to determine if two kangaroos meet.

---
+++ algorithms/kangaroo.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    # Rule 1: If one is ahead and moving faster/equal speed,
+    # the other can never catch up.
+    if v1 > v2:
+        # Rule 2: Check if the distance is divisible by speed difference
+        if (x2 - x1) % (v1 - v2) == 0:
+            return "YES"
+            
+    return "NO"         
+    
+    if v1 <= v2:
+        return "NO"
+        
+    # Check if they meet at the same position after some jumps
+    if (x2 - x1) % (v1 - v2) == 0:
+        return "YES"
+        
+    else:
+        return "NO"
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
